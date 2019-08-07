961d606ce324aad85dd24ac2e6bc4ad13531bc29
hsh3030 <hsh3030@gmail.com>
2019-08-07 17:30:46 +0900

    commit

--- ml/m16_cancer_keras_pipeline.py
+++ ml/m16_cancer_keras_pipeline.py
@@ -24,8 +24,7 @@ print(y.shape)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, train_size = 0.8, shuffle = True)
 
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
+
 print(y_train.shape)
 print(y_test.shape)
 
@@ -40,14 +39,15 @@ def build_network(keep_prob=0.5, optimizer='adam'):
     x = Dropout(keep_prob)(x)
     prediction = Dense(2, activation='softmax', name='output')(x)
     model = Model(inputs=inputs, output=prediction)
-    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
     return model
 
 def create_hyperparameters():
-    batches = [1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
-    optimizers =['rmsprop', 'adam', 'adadelta']
-    dropout = np.linspace(0.1, 0.5, 5)
-    return{"batch_size":batches, "optimizer":optimizers, "keep_prob":dropout}
+    return{
+    "svm__C": [1, 10, 100, 1000], "svm__kernel":["linear"],
+    "svm__C": [1, 10, 100, 1000], "svm__kernel":["rbf"], "svm__gamma":[0.001, 0.0001],
+    "svm__C": [1, 10, 100, 1000], "svm__kernel":["sigmoid"], "svm__gamma":[0.001, 0.0001]
+}
 
 from keras.wrappers.scikit_learn import KerasClassifier # classifier 분류
 model = KerasClassifier(build_fn=build_network, verbose=1) # 사이킥런으로 랩핑을 하다.
@@ -58,7 +58,12 @@ from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import KFold, GridSearchCV
 
 # estimator=> model을 가져온다.
-search = RandomizedSearchCV(estimator=model, param_distributions=hyperparameters,
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+
+pipe = Pipeline([("scaler", MinMaxScaler()), ('svm', SVC())])
+search = RandomizedSearchCV(estimator=pipe, param_distributions=hyperparameters,
                             n_iter=10, n_jobs=10, cv=5, verbose=1)# 작업이 n_iter = 10회 수행, cv = 3겹 교차 검증
 
 search.fit(x_train, y_train)
@@ -68,10 +73,7 @@ print(search.best_params_)
 print('score: ', search.score(x_test, y_test))
 print('predict: ', search.predict(x_test))
 
-###### best_params_ model 훈련 #######
-model_dic = search.best_params_
-
-model = build_network(model_dic['keep_prob'], model_dic['optimizer'])
-model.fit(x_train, y_train, batch_size=model_dic['batch_size'], epochs = 500)
-
-print("acc: ", model.evaluate(x_test, y_test))
+'''
+{'svm__kernel': 'sigmoid', 'svm__gamma': 0.001, 'svm__C': 1000}
+score:  1.0
+'''
